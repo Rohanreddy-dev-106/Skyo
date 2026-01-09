99fed4182f4f4d4f43359b1dbe34b9757a256ae9
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-01-09 17:25:57 +0530

    userManagement-completed/repo

--- Backend/src/User_Management/user.management.repo.js
+++ Backend/src/User_Management/user.management.repo.js
@@ -1,11 +1,79 @@
-/*  
-Filter by price 
+import flightmodel from "../Flights_Management/flight.schema.js";
 
-Filter by  Time 
+export default class Usermanagementrepo {
 
-Filter by sourc/destination
+    async FilterbyPrice(min, max) {
+        try {
+            const filter_data = await flightmodel.find({ $and: [{ price: { $gte: min } }, { price: { $lte: max } }] });
+            return filter_data;
+        }
+        catch (err) {
+            console.log(err.message);
+
+        }
+    }
+
+    async FilterbyTime(min, max) {
+        try {
+            const filter_data = await flightmodel.find({ $and: [{ startTime: { $gte: min } }, { endTime: { $lte: max } }] });
+            return filter_data;
+        }
+        catch (err) {
+            console.log(err.message);
+
+        }
+    }
+    async Filterbyplace(source, destination) {
+        try {
+            const filter_data = await flightmodel.find({ source, destination });
+            return filter_data;
+        }
+        catch (err) {
+            console.log(err.message);
+
+        }
+    }
+
+    async FilterbySets(maisets, maxsets) {
+        try {
+            const filter_data = await flightmodel.find({ $and: [{ totalseats: { $gte: maisets } }, { totalseats: { $lte: maxsets } }] });
+            return filter_data;
+        }
+        catch (err) {
+            console.log(err.message);
+
+        }
+    }
+
+    //Aggrection Apis for  admin dashboard
+    async AdminDashboard(skip, limit) {
+        try {
+            const filter_data = await flightmodel.aggregate([
+                {
+                    $group: {
+                        _id: "$airline",
+                        Flights: { $push: "$Flightname" },
+                        avgprise: { $avg: "$price" }
+                    }
+                },
+                {
+                    $sort: { avgprise: -1 },
+                },
+                {
+                    $skip: skip
+                },
+                {
+                    $limit: limit
+                }
+            ])
+            return filter_data;
+        } catch (error) {
+            console.log(error.message);
+
+        }
+    }
+}
 
 
-Filter by  Seats like  >50
 
-*/
+
