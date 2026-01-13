6e9ec15ed6a4edb16d2c06a922f0662a60305caf
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-01-14 03:54:35 +0530

    Bug-fix(foreach)

--- Backend/src/Bookings/booking.repository.js
+++ Backend/src/Bookings/booking.repository.js
@@ -5,12 +5,25 @@ import SeatsModel from "../Flights_Management/seats.schema.js";
 export default class BookingRepo {
     async creatbooking(data) {
         try {
-            const booking = new Bookingmodel({...data,Cost: 0});
+
+            const booking = new Bookingmodel({ ...data, Cost: 0 });
+            //Befor saving a validation check
+            let isAlreadyBooked = false;
+            let seatID = "";
+            const check = await SeatsModel.find({ _id: { $in: booking.Seat } });
+            check.forEach((value) => {//Logical Bug i made :When you use return inside a forEach callback, it only returns from the callback not from total function
+                if (value.status === "booked") {
+                    isAlreadyBooked = true;
+                    seatID = value._id
+                }
+            })
+            if (isAlreadyBooked) return { message: `${seatID} This seat is Booked by some one please Try another` }
 
             const bookingSaved = await booking.save();
 
             // Update booking cost
             const result = await this.updateBookingCost(bookingSaved._id);
+
 
             // Update all selected seats to booked
             if (bookingSaved.Seat && bookingSaved.Seat.length > 0) {//extra safty
@@ -34,9 +47,9 @@ export default class BookingRepo {
 
     async payment(data, Booking_id) {
         try {
-            const booking=await Bookingmodel.findById(Booking_id);
-            const amount=booking.Cost;
-            const payment = new Paymentmodel({...data,Amount:amount });
+            const booking = await Bookingmodel.findById(Booking_id);
+            const amount = booking.Cost;
+            const payment = new Paymentmodel({ ...data, Amount: amount });
 
             if (this.HelpingMethod()) {
                 payment.Status = "Success";
@@ -93,11 +106,11 @@ export default class BookingRepo {
                 select: "price -_id" // only price field in Seats, exclude _id
             });
 
-        if (!booking){
+        if (!booking) {
             return 0;
         }
 
-      
+
         const totalcost = booking.Seat.reduce((accumulator, value) => {//The accumulator stores the running result and gets updated with each iteration.
             return accumulator + value.price;
         }, 0);
@@ -125,20 +138,20 @@ export default class BookingRepo {
 
     //Admin api
 
-    async  Cancellation(bookingId, paymentId, seatIds = []) {
-    await Bookingmodel.findByIdAndDelete(bookingId);
+    async Cancellation(bookingId, paymentId, seatIds = []) {
+        await Bookingmodel.findByIdAndDelete(bookingId);
 
-    await Paymentmodel.findByIdAndDelete(paymentId);
+        await Paymentmodel.findByIdAndDelete(paymentId);
 
-    await SeatsModel.updateMany(
-        { _id: { $in: seatIds } },
-        {
-            $set: {
-                status: "available",
-                bookedBy: null
+        await SeatsModel.updateMany(
+            { _id: { $in: seatIds } },
+            {
+                $set: {
+                    status: "available",
+                    bookedBy: null
+                }
             }
-        }
-    );
-}
+        );
+    }
 
 }
