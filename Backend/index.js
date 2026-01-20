f9b62c52b6b5406b73faddd8bd82e98fab524eba
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-01-20 09:40:01 +0530

    urlencoder added

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const server=express();
-server.use(express.json())
+server.use(express.json())//Postman
+server.use(express.urlencoded({ extended: true }));//Data comming from HTML forms
 server.use(cookieParser())
 server.use(cors({
     origin: "*" // allow all origins
